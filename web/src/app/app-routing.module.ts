476b1205b852466acd46e2f9a6c4e8faaf5f23b0
loloslols <loloslols@nate.com>
2018-11-12 00:41:31 +0900

    회원가입 틀 만들고 라우팅 구현
    회원가입 틀 만들었고 로그인 창에서 회원가입 누르면 회원가입 창으로 이동, 회원가입 창에서 회원가입을 누르면 다시 홈으로 이동하게끔 라우팅함

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 const routes: Routes = [
   { path: '',  component: WelcomeComponent },
   { path: 'login', component: LoginComponent},
+  { path: 'signup', component: SignupComponent},
   
 ];
 
