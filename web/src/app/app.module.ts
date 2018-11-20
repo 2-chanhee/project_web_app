17e54d03c284f05c5c19f18093496cdad69e5967
One dohyeon <31878698+Onedoheyn@users.noreply.github.com>
2018-11-20 13:14:41 +0900

    복구 ?

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './user/user.component';
+import { ViewComponent } from './view/view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    AdminComponent,
+    UserComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule,
