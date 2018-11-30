91d6aac02d287225ee4992dff154b84de49fba47
loloslols <loloslols@nate.com>
2018-11-30 17:44:31 +0900

    서버 코드 일부 수정2
    서버 코드 일부 수정2

--- server/controllers/employeeController.js
+++ server/controllers/employeeController.js
@@ -2,8 +2,8 @@ const express = require('express');
 var router = express.Router();
 var ObjectId = require('mongoose').Types.ObjectId;
 var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
 var Book = require('../models/employee');
 
 // => localhost:3000/employees/
@@ -24,19 +24,25 @@ router.get('/:id', (req, res) => {
     });
 });
 
-router.post('/insert', (req, res) => {
-    var book = new Book({
-        category: req.body.category,
-        title: req.body.title,
-        content: req.body.content,
-        price: req.body.price,
-        imgurl: req.body.imgurl
-    });
-    book.save((err, doc) => {
-        if (!err) { res.send(doc); }
-        else { console.log('Error in Employee Save :' + JSON.stringify(err, undefined, 2)); }
+router.post('/insert', function(req, res,next){
+    var newBook = new Book();
+    newBook.category = req.body.category;
+    newBook.title = req.body.title;
+    newBook.content = req.body.content;
+    newBook.price = req.body.price;
+    newBook.imgurl = req.body.imgurl;
+    console.log("\n\n post req.body.title="+req.body.title);
+    newBook.save(function(err,docs){
+        if(err){
+            console.error(err);
+            res.json({result: 0});
+            return;
+        }else
+        res.send(docs);
     });
 });
+
+
 
 router.put('/:id', (req, res) => {
     if (!ObjectId.isValid(req.params.id))
