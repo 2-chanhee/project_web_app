08b321a6537bb532e28c1396838f6a0662983e9d
loloslols <loloslols@nate.com>
2018-12-08 22:45:27 +0900

    앱-서버 로그인 연동 성공
    앱-서버 로그인 연동 성공

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -17,14 +17,13 @@ var userSchema = new mongoose.Schema({
         required: 'Password can\'t be empty',
         minlength: [4, 'Password must be atleast 4 character long']
     },
-    saltSecret: String
 });
 // Events
-userSchema.pre('save', function (next) {
+/* userSchema.pre('save', function (next) {
     bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(this.password, salt, (err, hash) => {
             this.password = hash;
-            this.saltSecret = salt;
+            //this.saltSecret = salt;
             next();
         });
     });
@@ -38,11 +37,17 @@ userSchema.pre('put', function (next) {
             next();
         });
     });
-});
+}); */
 
 // Methods
 userSchema.methods.verifyPassword = function (password) {
-    return bcrypt.compareSync(password, this.password);
+    
+    if(password==this.password){
+        return password;
+    }
+   else{
+       return false;
+   }
 };
 
 userSchema.methods.generateJwt = function () {
