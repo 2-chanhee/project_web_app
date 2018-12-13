ceda06b02f0dfa3993c7aaeda6bdbd7684742fd1
One dohyeon <31878698+Onedoheyn@users.noreply.github.com>
2018-12-13 15:09:30 +0900

    웹
    회원가입 완성

--- web/src/app/user/sign-up/sign-up.component.ts
+++ web/src/app/user/sign-up/sign-up.component.ts
@@ -1,6 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm} from '@angular/forms';
-import {UserService} from '../../shared/user.service';
+import { Component,OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from "@angular/router";
+
+import { UserService } from '../../shared/user.service';
+
+import { User } from '../../shared/user.model';
+declare var M: any;
+
 
 @Component({
   selector: 'app-sign-up',
@@ -8,6 +14,8 @@ import {UserService} from '../../shared/user.service';
   styleUrls: ['./sign-up.component.css'],
   providers : [UserService]
 })
+
+
 export class SignUpComponent implements OnInit {
   emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   showSuccessMessage: boolean;
@@ -16,32 +24,61 @@ export class SignUpComponent implements OnInit {
   constructor(private userService: UserService) { }
 
   ngOnInit() {
-  }
-  onSubmit(form : NgForm){
-    this.userService.postUser2(form.value).subscribe(
-      res => {
-        alert('회원가입 완료');
-        this.resetForm(form);
-      },
-      err => {
-        if(err.status == 422){
-          alert('이메일 중복');
-        } 
-        else
-          alert('db연결 오류');
-      }
-    );
+    this.resetForm();
+    this.refreshUserList();
   }
 
-  resetForm(form: NgForm){
+  
+  resetForm(form?: NgForm) {
+    if (form)
+      form.reset();
     this.userService.selectedUser = {
-      _id:'',
-      fullName: '',
-      email: '',
-      password: ''
-    };
-    form.resetForm();
-    this.serverErrorMessages = '';
+      _id: "",
+      fullName: "",
+      email: "",
+      password: ""
+    
+    }
   }
+
+  onSubmit(form: NgForm) {
+    if (form.value._id == "") {
+      this.userService.postUser2(form.value).subscribe((res) => {
+        this.resetForm(form);
+        this.refreshUserList();
+        M.toast({ html: 'Saved successfully', classes: 'rounded' });
+      });
+    }
+    else {
+      this.userService.putUser(form.value).subscribe((res) => {
+        this.resetForm(form);
+        this.refreshUserList();
+        M.toast({ html: 'Updated successfully', classes: 'rounded' });
+      });
+    }
+  }
+
+  refreshUserList() {
+    this.userService.getUserList().subscribe((res) => {
+      this.userService.users = res as User[];
+    });
+  }
+  onEdit(user: User ,id: string) {
+    this.userService.selectedUser = user;
+    var check = localStorage.setItem('check', id);
+  }
+
+  onDelete(_id: string, form: NgForm) {
+    if (confirm('Are you sure to delete this record ?') == true) {
+      this.userService.deleteUser(_id).subscribe((res) => {
+        this.refreshUserList();
+        this.resetForm(form);
+
+      
+       
+      });
+    }
+  }
+
 
 }
